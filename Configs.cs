8e2c8927baa65b48e071bb09a4b21416ac3627d8
sneakykestrel <141267380+sneakykestrel@users.noreply.github.com>
2025-01-22 17:04:48 +0000

    Initial Commit

---
+++ Configs.cs
@@ -0,0 +1,71 @@
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace CrosshairOverlay;
+
+public static class Configs
+{
+    public static ConfigEntry<FilterMode> filteringMode;
+    public static ConfigEntry<float> width;
+    public static ConfigEntry<float> height;
+    public static ConfigEntry<float> offsetX;
+    public static ConfigEntry<float> offsetY;
+    public static ConfigEntry<bool> overlay;
+    public static ConfigEntry<bool> hideDefault;
+    public static ConfigEntry<bool> hideInMenus;
+    public static ConfigEntry<bool> hideWhenScoped;
+    public static ConfigEntry<string> filename;
+    
+    public static ConfigEntry<byte> tintR;
+    public static ConfigEntry<byte> tintG;
+    public static ConfigEntry<byte> tintB;
+    public static ConfigEntry<byte> tintA;
+
+    public static void BindTo(ConfigFile config) {
+        filename = config.Bind("General",
+            "File name",
+            "default-crosshair.png",
+            $"The name of the image file to load. png and jpeg formats are supported, and the file should be placed in the mod's directory. ({Mod.PluginPath})"
+        );
+        overlay = config.Bind(
+            "General",
+            "Overlay",
+            true,
+            "Wether the crosshair image should be on top of the UI. If disabled, the image will be under the UI. (NOTE: this does not include the 3D version of the player HUD~ the crosshair will always be above it.)"
+        );
+        filteringMode = config.Bind(
+            "General",
+            "Image Filter Mode",
+            FilterMode.Point,
+            "The texture filtering mode to use for the crosshair image."
+        );
+        hideDefault = config.Bind(
+            "General",
+            "Hide default crosshair",
+            true,
+            "Whether to hide the default crosshair~ this option uses the behaviour of ctrl+k, so the crosshair can be reenabled at any time."
+        );
+        hideInMenus = config.Bind(
+            "General",
+            "Hide in menu",
+            true,
+            "Whether to hide the custom crosshair when in the main menu."
+        );
+        hideWhenScoped = config.Bind(
+            "General",
+            "Hide when scoped",
+            true,
+            "Whether to hide the custom crosshair when aiming with a scoped weapon."
+        );
+        
+        width = config.Bind("Style.Size", "Width", 30f, "The width of the crosshair image.");
+        height = config.Bind("Style.Size", "Height", 30f, "The height of the crosshair image.");
+        offsetX = config.Bind("Style.Offset", "X Offset", 0f, "The X offset of the crosshair image.");
+        offsetY = config.Bind("Style.Offset", "Y Offset", 0f, "The Y offset of the crosshair image.");
+        
+        tintR = config.Bind("Style.Color", "1 Tint R", (byte)255, new ConfigDescription("The red channel tint of the crosshair image.", new AcceptableValueRange<byte>(0, 255)));
+        tintG = config.Bind("Style.Color", "2 Tint G", (byte)255, new ConfigDescription("The green channel tint of the crosshair image.", new AcceptableValueRange<byte>(0, 255)));
+        tintB = config.Bind("Style.Color", "3 Tint B", (byte)255, new ConfigDescription("The blue channel tint of the crosshair image.", new AcceptableValueRange<byte>(0, 255)));
+        tintA = config.Bind("Style.Color", "4 Tint A", (byte)255, new ConfigDescription("The alpha channel tint of the crosshair image.", new AcceptableValueRange<byte>(0, 255)));
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace CrosshairOverlay;
+
+[BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+public class Mod : BaseUnityPlugin
+{
+    public const string pluginGuid = "kestrel.straftat.crosshairoverlay";
+    public const string pluginName = "Crosshair Overlay";
+    public const string pluginVersion = "1.1.1";
+    
+    public static Mod Instance { get; private set; }
+    internal static new ManualLogSource Logger;
+
+    public static string PluginPath { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+    static string imagePath;
+    static Sprite overlaySprite;
+    static GameObject root;
+    static Image imageComponent;
+    static RectTransform transformComponent;
+    static bool queueImageReload;
+    static bool imageLoaded;
+    
+    static string loadBearingColonThree = ":3";
+    private void Awake() {
+        if (loadBearingColonThree != ":3") Application.Quit();
+        gameObject.hideFlags = HideFlags.HideAndDontSave;
+        Instance = this;
+        Logger = base.Logger;
+        
+        Configs.BindTo(Config);
+
+        Config.SettingChanged += OnConfigChanged;
+        imagePath = Path.Combine(PluginPath, Configs.filename.Value);
+        
+        if (!File.Exists(imagePath)) 
+            Logger.LogWarning($"No image file found at {imagePath}");
+        else 
+            StartCoroutine(LoadCrosshairImage());
+        
+        new Harmony(pluginGuid).PatchAll();
+        Logger.LogInfo("Hiiiiiiiiiiii :3");
+    }
+
+    void OnConfigChanged(object sender, SettingChangedEventArgs e) {
+        queueImageReload = true;
+        if (e.ChangedSetting != Configs.filename) return;
+        imagePath = Path.Combine(PluginPath, Configs.filename.Value);
+        if (!File.Exists(imagePath)) {
+            Logger.LogWarning($"No image file found at {imagePath}");
+            return;
+        }
+
+        StartCoroutine(LoadCrosshairImage());
+    }
+
+    static IEnumerator LoadCrosshairImage() {
+        imageLoaded = false;
+        using var uwr = UnityWebRequestTexture.GetTexture("file:///" + Path.Combine(PluginPath, Configs.filename.Value));
+        
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result != UnityWebRequest.Result.Success) {
+            // Panic!!!!!1!
+            Logger.LogError("Error while loading overlay image ~ " + uwr.error + $"(the file at {imagePath} is probably invalid.)");
+        } else {
+            Logger.LogInfo("Found and loaded crosshair overlay!");
+            var texture = DownloadHandlerTexture.GetContent(uwr);
+            overlaySprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            imageLoaded = true;
+        }
+    }
+
+    static void SetupCrosshair(Transform uiRoot = null) {
+        root = new GameObject("Crosshair Overlay");
+        transformComponent = root.AddComponent<RectTransform>();
+        var group = root.AddComponent<CanvasGroup>();
+        group.blocksRaycasts = false;
+        imageComponent = root.AddComponent<Image>();
+        if (uiRoot) root.transform.SetParent(uiRoot, false);
+    }
+
+    static void ReloadCrosshair() {
+        transformComponent.sizeDelta = new Vector2(Configs.width.Value/2f, Configs.height.Value/2f);
+        transformComponent.anchoredPosition = new Vector2(Configs.offsetX.Value, Configs.offsetY.Value);
+        imageComponent.color = new Color32(Configs.tintR.Value, Configs.tintG.Value, Configs.tintB.Value, Configs.tintA.Value);
+        overlaySprite.texture.filterMode = Configs.filteringMode.Value;
+        imageComponent.sprite = overlaySprite;
+        if (!Configs.overlay.Value) root.transform.SetAsFirstSibling();
+        else root.transform.SetAsLastSibling();
+    }
+    
+    [HarmonyPatch(typeof(Crosshair))]
+    public static class CrosshairPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        public static void SetupCrosshairOverlay(Crosshair __instance) {
+            if (Configs.hideDefault.Value) __instance.transform.localScale = Vector3.zero;
+            SetupCrosshair(__instance.transform.parent);
+            if (imageLoaded) ReloadCrosshair();
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        public static void UpdateOverlayVisibility(ref Crosshair __instance) {
+            if (queueImageReload && imageLoaded) {
+                ReloadCrosshair();
+                queueImageReload = false;
+            }
+            
+            if (Configs.hideWhenScoped.Value && __instance.player is not null && __instance.canScopeAim && __instance.player.isAiming) {
+                root.SetActive(false);
+            }
+            else if (Configs.hideInMenus.Value && SceneManager.GetActiveScene().name == "MainMenu") {
+                root.SetActive(false);
+            }
+            else root.SetActive(true);
+        }
+    }
+}
+
